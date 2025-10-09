b5a0c8d8929f1fe0b7cdb838f9e61496c44cda26
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-10-09 15:09:55 +0530

    new programs are added  day 10 added,,,,,

---
+++ Python Dev/class & object/classobject.py
@@ -0,0 +1,5 @@
+class python:
+    def mack(self):
+        print("mock is belong to python")
+p = python()
+p.mack()
--- Python Dev/generator/generator.py
+++ Python Dev/generator/generator.py
@@ -1,2 +1,23 @@
 #generator is function
 # used to create iterators
+from fontTools.misc.cython import returns
+
+
+def get_fun():
+    for i in range(1,11):
+        yield i
+for i in get_fun():
+    print(i)
+
+def new_Str():
+    a = "siddharth"
+    yield a
+    b= "Deepak"
+    yield b
+    c= "shinde"
+    # return a+" "+b+" "+c
+    yield c
+n = new_Str()
+print(next(n))
+print(next(n))
+print(next(n))
