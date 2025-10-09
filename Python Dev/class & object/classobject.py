86dc06fa730298ad60e1f5cfbd6d70db83269a9f
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-10-09 15:33:14 +0530

    new programs are added  added,,,,,

--- Python Dev/class & object/classobject.py
+++ Python Dev/class & object/classobject.py
@@ -1,7 +1,18 @@
 class python:
+    id=input("Enter the id : ")
+
+    name=input("Enter your Name : ")
     def mack(self):
         print("mock is belong to python")
+
+    def test(self):
+        print("test belong to python")
 p = python()
-p2 = python()
-p.mack()
-p2.mack()
+print("my id is "+p.id)
+print("my name23 is "+p.name)
+# p2 = python()
+
+# p.mack()
+p.test()
+python.mack(p)
+# p2.mack()
