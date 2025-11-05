efea1a86cb327a1d6c33a0870a890057a3e436c6
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-11-05 16:53:50 +0530

    new programs are added  added,,,,,

--- Python Dev/database_connectivity/newdb.py
+++ Python Dev/database_connectivity/newdb.py
@@ -2,6 +2,24 @@ import sqlite3 as s
 
 con = s.connect("cravita.db")
 cur = con.cursor()
-cur.execute("create table fortune (id int(10),name varchar(20))")
+# cur.execute("create table fortune (id int(10),name varchar(20))")
+# cur.execute("insert into fortune(id,name) values(1,'Python') ")
+# cur.execute("insert into fortune values(2,'sql')")
+# cur.execute("delete from fortune where id = '2'")
+
+# cur.execute("insert into fortune(id,name) values(?,?)",(3,'abc'))  #for flask we insert data in that way
+
+# a = [(4,'Java'),(5,'sql'),(6,'js')]
+# cur.executemany("insert into fortune(id,name) values(?,?)",a)
+
+# data = cur.execute("select * from fortune")
+#
+# print(data)
+# for row in data:
+#     print(row)
+
+cur.execute("select * from fortune where id = ?",[4])
+data = cur.fetchone()
+print(data)
 con.commit()
 con.close()
--- Python Dev/day2/for1.py
+++ Python Dev/day2/for1.py
@@ -2,7 +2,7 @@
 
 n = int(input("enter the number "))
 # fact= 1
-for i in range(1 ,n, 1):
+for i in range(1 ,n, 2):
     print(n-i)
 
 
