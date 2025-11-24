b08aab91d4ff4dd7468ded9bd4278de79c626798
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-11-24 16:09:09 +0530

    new files added

--- Python Dev/polymorphismm/method_overload.py
+++ Python Dev/polymorphismm/method_overload.py
@@ -1,12 +1,12 @@
 class python:
-    def add1(self,n1,n2,):
-        print(n1+n2)
+    def add1(self,n3,n4,):
+        print(n3+n4)
 
     def add1(self,n1,n2,n3):
         print(n1,n2,n3)
 
 p=python()
 
-p.add1(n1=30,n2=3)
+p.add1(n3=30,n4=3)
 p.add1(10,20,3)
 
